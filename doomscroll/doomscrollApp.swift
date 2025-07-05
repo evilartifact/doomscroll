059baec1c5cc4ca71ded10669964884735db8391
Rob <rabinaryal530@gmail.com>
2025-07-05 12:47:18 -0400

    Initial Commit

---
+++ doomscroll/doomscrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  doomscrollApp.swift
+//  doomscroll
+//
+//  Created by Rabin on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct doomscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ doomscrollTests/doomscrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  doomscrollTests.swift
+//  doomscrollTests
+//
+//  Created by Rabin on 7/5/25.
+//
+
+import Testing
+@testable import doomscroll
+
+struct doomscrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
